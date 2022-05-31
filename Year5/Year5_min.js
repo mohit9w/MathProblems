e297e0603b5d4fecb9abf259ee88673d27dab711
mrodey <mrodey@bravurasolutions.com>
2022-05-31 19:20:14 +1000

    year5 min js updated with latest

--- Year5/Year5_min.js
+++ Year5/Year5_min.js
@@ -1,5 +1,6 @@
 var arrVal=0;var chosenVal=0;$(function(){populateYear5NavItems();hideConceptDiv();});function populateYear5NavItems(){$('#navBtns').empty();$('#navigationMenu').show();let navBtnData=fetchJSONObject(_selectedYr);$.each(navBtnData,function(key,value){$.each(value,function(index,json){if(json.enabled==true){$(json.addTo).append('<button type=\"button\" class=\"btn btn-outline-warning\" onClick=\"'+json.clickFunc+'\">'+json.name+'</button> &nbsp;');}else{$(json.addTo).append('<button type=\"button\" class=\"btn btn-outline-warning disabled\" data-bs-toggle=\"tooltip\" data-bs-placement=\"bottom\" title=\"WORK In Progress !!\">'+json.name+'</button> &nbsp;');}});});enableAllTooltips();}
 function reload(){resetAllParams();}
+function getCheckAnswerButton(){return $('#chkans');}
 function getDdMenu(){return $('#ddMenu');}
 function getChooseMenu(){return $('#choose');}
 function getLoadQuestionsButton(){return $('#lqst');}
@@ -8,13 +9,13 @@ function hideConceptDiv(){$('#conceptDiv').hide();}
 function showConceptDiv(){$('#conceptDiv').show();}
 function setResult(className,resultText){let divPointer=getResultDiv();divPointer.prop("className",className);divPointer.prop("innerHTML","<strong>"+resultText+"</strong>");divPointer.prop("style","display:block");}
 function enableChooseMenu(val){let divPointer=getChooseMenu();divPointer.prop("className","btn btn-primary dropdown-toggle");divPointer.prop("innerHTML","Choose "+val+" Type :");}
-function clearDdMenu(){$('#ddMenu').empty();$('#choose').html("Choose Option:").addClass("btn btn-secondary dropdown-toggle disabled");}
+function clearDdMenu(){getDdMenu().empty();getChooseMenu().html("Choose Option:").prop("className","btn btn-secondary dropdown-toggle disabled");}
 function enableLoadQuestionsButton(){getLoadQuestionsButton().prop("className","btn btn-primary btn-sm p-3 mt-2");}
-function disableLoadQuestionsButton(){getLoadQuestionsButton().prop("className","btn btn-primary btn-sm disabled p-3 mt-2");}
-function disableChkAnsButton(){$('#chkans').prop("className","btn btn-secondary btn-sm disabled p-3 mt-2");}
+function disableLoadQuestionsButton(){getLoadQuestionsButton().prop("className","btn btn-secondary btn-sm disabled p-3 mt-2");}
+function disableChkAnsButton(){getCheckAnswerButton().prop("className","btn btn-secondary btn-sm disabled p-3 mt-2");}
 function enableChkAnsButton(){$("#chkans").prop("className","btn btn-primary btn-sm p-3 mt-2");}
 function clearAllQuestions(){$('#questions').prop("innerHTML","");}
-function resetAllParams(){hideConceptDiv();clearDdMenu();reset();hideResult();clearAllQuestions();$('#lqst').addClass("btn btn-secondary btn-sm disabled p-3 mt-2");$('#chkans').addClass("btn btn-secondary btn-sm disabled p-3 mt-2");$("#output").html("");}
+function resetAllParams(){hideConceptDiv();clearDdMenu();reset();hideResult();clearAllQuestions();disableLoadQuestionsButton();disableChkAnsButton();$("#output").html("");}
 function displayChoose(val){resetAllParams();let ddMenu=getDdMenu();for(i=0;i<getSelectionLength(val);i++){if(val=="Division"){ddMenu.append("<a class=\"dropdown-item\" onClick=\"loadDivisionParams(4,"+i+");\">"+fetchJSONObject(landingMenuLabel).divisionDisplay[i]+"</a></li>");}else if(val=="Addition"){ddMenu.append("<a class=\"dropdown-item\" onClick=\"loadAdditionParams(1,"+i+");\">"+fetchJSONObject(landingMenuLabel).commonDisplay[i]+"</a></li>");}else if(val=="Subtraction"){ddMenu.append("<a class=\"dropdown-item\" onClick=\"loadSubtractionParams(2,"+i+");\">"+fetchJSONObject(landingMenuLabel).divisionDisplay[i]+"</a></li>");}else if(val=="Multiplication"){ddMenu.append("<a class=\"dropdown-item\" onClick=\"loadMultiplicationParams(3,"+i+");\">"+fetchJSONObject(landingMenuLabel).multiplicationDisplay[i]+"</a></li>");}}
 enableChooseMenu(val);}
 function getSelectionLength(val){if(val=="Division"||val=="Subtraction"){return fetchJSONObject(landingMenuLabel).divisionDisplay.length;}else if(val=="Addition"){return fetchJSONObject(landingMenuLabel).commonDisplay.length;}else if(val=="Multiplication"){return fetchJSONObject(landingMenuLabel).multiplicationDisplay.length;}}
@@ -34,5 +35,5 @@ if(correct==maxQ){showResult(100,"Huraaaaaaayyyy! You Got 100%.");}else if(corre
 disableChkAnsButton();}
 function getResult(i){if(chosenVal==1){return(num1[i]+num2[i]);}else if(chosenVal==2){return(num1[i]-num2[i]);}else if(chosenVal==3){return(num1[i]*num2[i]);}else if(chosenVal==4){if((num1[i]%num2[i])==0){return(num1[i]/num2[i]);}else{return(num1[i]/num2[i]).toFixed(2);}}}
 function showResult(res,resultText){if(res==100){setResult("alert alert-success",resultText);}else if(res<50){setResult("alert alert-danger",resultText);}else{setResult("alert alert-info",resultText);}}
-function hideResult(){var x=$("#resultDiv").hide();}
+function hideResult(){var x=getResultDiv().hide();}
 function disableAllYear5Questions(){$('#questions :input').attr('disabled',true);}
